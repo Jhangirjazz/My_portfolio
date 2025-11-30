1e5b927b132c42bb8fbd1fa7b612dc41f20d0469
jhangir khan <jhangirjazz590@gmail.com>
2025-11-30 20:52:02 +0500

    Update resume file and ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/Muhammad-Jahangir.pdf";
-import pdf from "../../Assets/Muhammad-Jahangir.pdf";
+import pdf from "../../Assets/Muhammad_Jahangir.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
